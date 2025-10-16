a3cb55e68c72d7153ccf938249fd8fe4149cfd10
princesskylaledesma <princesskylaledesma@gmail.com>
2025-10-16 22:03:01 +0800

    Add files via upload
    Uploaded Java case problem and output screenshot.

---
+++ Princess Kyla M. Ledesma_2C/CaseProblem.java
@@ -0,0 +1,18 @@
+public class CaseProblem {
+    public static void main(String[] args) {
+        
+        String name = "Princess Kyla M. Ledesma";
+        String schoolID = "2024303357";
+
+
+        int num1 = 100;
+        int num2 = 5;
+        int product = num1 * num2;
+
+        
+        System.out.println("Name: " + name);
+        System.out.println("ID Number: " + schoolID);
+        System.out.println("------------------------------");
+        System.out.println("Result: The product of " + num1 + " and " + num2 + " is " + product);
+    }
+}
